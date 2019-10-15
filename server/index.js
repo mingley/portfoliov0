6f0dae822d48b6d8c4d712c0507eaac1d68b4d18
mikasd <michael.ingley@gmail.com>
2019-10-15 11:35:45 -0700

    added dependencies and added api email route

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const numCPUs = require('os').cpus().length;
 const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
 
+const bodyParser = require('body-parser');
+const sendGrid = require('@sendGrid/mail');
+require('dotenv').config();
+
 // Multi-process to utilize all CPU cores.
 if (!isDev && cluster.isMaster) {
   console.error(`Node cluster master ${process.pid} is running`);
@@ -30,6 +34,29 @@ if (!isDev && cluster.isMaster) {
     res.set('Content-Type', 'application/json');
     res.send('{"message":"Hello from the custom server!"}');
   });
+
+  app.post('/api/email', (req, res, next) => {
+    console.log('called recieved in backend');
+    sendGrid.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: 'michael.ingley@gmail.com',
+      from: req.body.email,
+      subject: 'portfolio site contact from  ' + req.body.name,
+      text: req.body.message,
+    };
+    sendGrid.send(msg)
+      .then(result => {
+        res.status(200).json({
+          success: true
+        })
+      })
+      .catch(err => {
+        console.log('error====>', err);
+        res.status(401).json({
+          success: false
+        })
+      })
+  })
 
   // All remaining requests return the React app, so it can handle routing.
   app.get('*', function(request, response) {
